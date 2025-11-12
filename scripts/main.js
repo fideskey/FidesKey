7a7de5e9c727605bb7fc4811adc3022f069a2195
fideskey <fideskey@outlook.com>
2025-11-12 17:11:32 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import '../i18n.js'  // Import desde la raíz
+import App from '../App.js'  // ← ÚNICO CAMBIO
+import '../i18n.js'
 
 // Renderizar la aplicación React
 const container = document.getElementById('root')
