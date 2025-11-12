b4a47aaffcec8b91a4e441ea2c9e1c33ae290806
fideskey <fideskey@outlook.com>
2025-11-12 17:01:38 +0100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './i18n.js'
+
+// Renderizar la aplicación React
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
